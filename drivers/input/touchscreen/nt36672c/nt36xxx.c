c339a05b6be707f0ee897e9c8ce4ad346c8dd563
Jebaitedneko <Jebaitedneko@gmail.com>
2021-07-11 04:29:11 +0530

    input: touchscreen: nt36672c: squash of improvements
    input: touchscreen: nt36672c: nt36xxx: implement alternative for dsi_panel_lockdown_info_read()
    
     using nvt_cmds_panel_info() we can simply check the panel vendor
     and set lockdown data accordingly. this removes the extra code in
     drm changes so as to minimize bloat.
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    
    input: touchscreen: nt36672c: kill logspam
    
    input: touchscreen: nt36672c: crc check fixups
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    
    input: touchscreen: nt36672c: relax esd checking a bit
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    
    input: touchscreen: nt36672c: set esd checking period to 125ms
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    
    input: touchscreen: nt36672c: implement esd check override
    
    node: /sys/module/nt36xx/parameters/touch_fw_override
    
    values:
    
    0: no esd checking (default)
    1: force esd check
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    
    input: touchscreen: nt36672c: implement esd check duration scaling
    
    node: /sys/module/nt36xx/parameters/esd_check_scale
    
    values: 4-24
    
    minimum: 4 * NVT_TOUCH_ESD_CHECK_PERIOD (for extreme cases)
    default: 8 * NVT_TOUCH_ESD_CHECK_PERIOD (minimum)
    maximum: 24 * NVT_TOUCH_ESD_CHECK_PERIOD
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    
    input: touchscreen: nt36xxx: enter deep sleep state
    
    To avoid curret leakage, force touch IC to enter deep sleep state.
    
    [Jebaitedneko]: Modify cmds for SPI mode
    
    Change-Id: Icf6ef85806980e3881f0fabc3c0b47d49f0e6392
    Signed-off-by: Fei Mao <feim1@codeaurora.org>
    Co-authored-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    
    input: touchscreen: nt36672c: implement firmware override
    
    node: /sys/module/nt36xx/parameters/touch_fw_override
    
    values:
    
    [Tianma/36_02_0a]
    1:  j20s_novatek_ts_fw01.2f0c.bin
    2:  j20s_novatek_ts_fw01.2fa7.bin
    3:  j20s_novatek_ts_fw01.3a42.bin
    4:  j20s_novatek_ts_fw01.8ac8.bin
    
    [Huaxing/42_02_0b]
    5:  j20s_novatek_ts_fw02.71e7.bin
    6:  j20s_novatek_ts_fw02.9874.bin
    7:  j20s_novatek_ts_fw02.fd13.bin
    
    [Tianma(MP)/36_02_0a]
    8:  j20s_novatek_ts_mp01.1b3b.bin
    9:  j20s_novatek_ts_mp01.8cb5.bin
    
    [Huaxing(MP)/42_02_0b]
    10: j20s_novatek_ts_mp02.2c1f.bin
    11: j20s_novatek_ts_mp02.9fb2.bin
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    
    input: touchscreen: nt36672c: cache panel vendor
    
    reduces strncmp and memcpy load via multiple calls to nvt_cmds_panel_info()
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    
    input: touchscreen: nt36672c: prevent incorrect fw loading
    
    check panel_is_tianma for panel detection
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    
    input: touchscreen: nt36672c: log firmware reloading only
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    
    input: touchscreen: xiaomi: xiaomi_touch: remove unused get_xiaomi_touch_{class,dev}
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- drivers/input/touchscreen/nt36672c/nt36xxx.c
+++ drivers/input/touchscreen/nt36672c/nt36xxx.c
@@ -63,6 +63,8 @@ static struct workqueue_struct *nvt_esd_check_wq;
 static unsigned long irq_timer = 0;
 uint8_t esd_check = false;
 uint8_t esd_retry = 0;
+static int esd_check_force = 0;
+static int esd_check_scale = 8;
 #endif /* #if NVT_TOUCH_ESD_PROTECT */
 
 #if NVT_TOUCH_EXT_PROC
@@ -94,7 +96,6 @@ static void nvt_ts_late_resume(struct early_suspend *h);
 #endif
 static int32_t nvt_ts_suspend(struct device *dev);
 static int32_t nvt_ts_resume(struct device *dev);
-extern int dsi_panel_lockdown_info_read(unsigned char *plockdowninfo);
 extern void dsi_panel_doubleclick_enable(bool on);
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
 static int32_t nvt_check_palm(uint8_t input_id, uint8_t *data);
@@ -1161,9 +1162,36 @@ static bool nvt_cmds_panel_info(void)
 		if (!strncmp(display_node, "dsi_j20s_36_02_0a_video_display",
 					strlen("dsi_j20s_36_02_0a_video_display"))) {
 			panel_id = true;
+			panel_is_tianma = 1;
 		}
 	}
 	return panel_id;
+}
+
+static inline int dsi_panel_lockdown_info_read(unsigned char *plockdowninfo)
+{
+	if (nvt_cmds_panel_info()) {
+		NVT_LOG("%s: lockdown panel is tianma\n", __func__);
+		plockdowninfo[0] = 0x46;
+		plockdowninfo[1] = 0x36;
+		plockdowninfo[2] = 0x32;
+		plockdowninfo[3] = 0x01;
+		plockdowninfo[4] = 0x4a;
+		plockdowninfo[5] = 0x14;
+		plockdowninfo[6] = 0x31;
+		plockdowninfo[7] = 0x00;
+	} else {
+		NVT_LOG("%s: lockdown panel is huaxing\n", __func__);
+		plockdowninfo[0] = 0x00;
+		plockdowninfo[1] = 0x00;
+		plockdowninfo[2] = 0x00;
+		plockdowninfo[3] = 0x00;
+		plockdowninfo[4] = 0x00;
+		plockdowninfo[5] = 0x00;
+		plockdowninfo[6] = 0x00;
+		plockdowninfo[7] = 0x00;
+	}
+	return 1;
 }
 
 static int nvt_get_panel_type(struct nvt_ts_data *ts_data)
@@ -1301,8 +1329,10 @@ bool nvt_get_dbgfw_status(void)
 }
 
 #if NVT_TOUCH_ESD_PROTECT
+module_param_named(esd_check_force, esd_check_force, int, 0664);
 void nvt_esd_check_enable(uint8_t enable)
 {
+	enable = esd_check_force;
 	/* update interrupt timer */
 	irq_timer = jiffies;
 	/* clear esd_retry counter, if protect function is enabled */
@@ -1327,11 +1357,18 @@ static uint8_t nvt_fw_recovery(uint8_t *point_data)
 	return detected;
 }
 
+module_param_named(esd_check_scale, esd_check_scale, int, 0664);
 static void nvt_esd_check_func(struct work_struct *work)
 {
 	unsigned int timer = jiffies_to_msecs(jiffies - irq_timer);
 
-	if ((timer > NVT_TOUCH_ESD_CHECK_PERIOD) && esd_check) {
+	if (esd_check_scale > 24)
+		esd_check_scale = 24;
+	if (esd_check_scale < 4)
+		esd_check_scale = 4;
+
+	if ((timer > esd_check_scale * NVT_TOUCH_ESD_CHECK_PERIOD + 50)
+		&& (timer < 2 * esd_check_scale * NVT_TOUCH_ESD_CHECK_PERIOD - 50) && esd_check) {
 		mutex_lock(&ts->lock);
 		NVT_LOG("do ESD recovery, timer = %d, retry = %d\n", timer, esd_retry);
 		/* do esd recovery, reload fw */
@@ -2657,6 +2694,7 @@ static int32_t nvt_ts_probe(struct platform_device *pdev)
 		debugfs_create_file("touch_boost", 0660, ts->debugfs, ts, &nvt_touch_test_fops);
 	}
 #endif
+	nvt_cmds_panel_info();
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
 			xiaomi_touch_interfaces.touch_vendor_read = nvt_touch_vendor_read;
 			xiaomi_touch_interfaces.panel_display_read = nvt_panel_display_read;
@@ -2937,6 +2975,11 @@ static int32_t nvt_ts_suspend(struct device *dev)
 	} else {
 		/* ---write command to enter "deep sleep mode"--- */
 		buf[0] = EVENT_MAP_HOST_CMD;
+		buf[1] = 0x11;
+		CTP_SPI_WRITE(ts->client, buf, 2);
+
+		nvt_set_page(0x11a50);
+		buf[0] = 0x11a50 & 0xff;
 		buf[1] = 0x11;
 		CTP_SPI_WRITE(ts->client, buf, 2);
 		if (ts->ts_pinctrl) {
@@ -3278,5 +3321,6 @@ static void __exit nvt_driver_exit(void)
 }
 late_initcall(nvt_driver_init);
 
+module_param_named(touch_fw_override, touch_fw_override, int, 0664);
 MODULE_DESCRIPTION("Novatek Touchscreen Driver");
 MODULE_LICENSE("GPL");
